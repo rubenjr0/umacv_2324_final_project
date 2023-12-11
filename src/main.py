4cc44eab5319ce19f0100832a9c3d24f615c3e91
Rubén <rubenjr0@protonmail.com>
2023-12-11 20:11:27 +0100

    ✨ rerun visualizations

--- src/main.py
+++ src/main.py
@@ -2,23 +2,32 @@ import cv2
 import numpy as np
 import rerun as rr
 
-from vision import _load_image, _fix_perspective, _segment_colors, _get_face_colors
+from vision import (
+    _preprocess,
+    _fix_perspective,
+    _segment_colors,
+    _get_face_colors,
+    _threshold_channel,
+)
 from solver import solve
 
-rr.init("rerun_example_demo", spawn=False)
+rr.init("rerun_example_demo", spawn=True)
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 
 while cap.isOpened():
     _, frame = cap.read()
-    cv2.imshow("frame", frame)
-    fixed, M = _fix_perspective(frame)
+    frame = _preprocess(frame)
+    rr.log("image/rgb", rr.Image(frame))
+    try:
+        fixed = _fix_perspective(frame)
+        rr.log("transformed", rr.Image(fixed))
+    except Exception as e:
+        print(e)
+        continue
     segmented = _segment_colors(fixed, boost=True)
+    rr.log("transformed/segmented", rr.Image(segmented))
     colors, centers = _get_face_colors(segmented)
     action = solve(colors)
-    print(M)
     print(colors)
     print(action)
-    rr.log("image", rr.Image(frame))
-    rr.log("transformed", rr.Image(fixed))
-    rr.log("transformed/segmented", rr.Image(segmented))
