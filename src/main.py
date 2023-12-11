545ef042744a948440120f18d5f7a1d85fd9bed8
Rubén J <rubenjr@protonmail.com>
2023-12-11 12:18:02 +0100

    🎨 Improved main structure

--- src/main.py
+++ src/main.py
@@ -2,16 +2,18 @@ import rerun as rr
 from vision import _load_image, _fix_perspective, _segment_colors, _get_face_colors
 from solver import solve
 
+rr.init("rerun_example_demo", spawn=True)
+
+
 sample = _load_image("data/1.png")
 fixed, M = _fix_perspective(sample)
 segmented = _segment_colors(fixed, boost=True)
 colors, centers = _get_face_colors(segmented)
 action = solve(colors)
 print(M)
-print(centers, centers.shape)
+print(colors)
 print(action)
 
-rr.init("rerun_example_demo", spawn=True)
 
 rr.log("image", rr.Image(sample))
 rr.log("transformed", rr.Image(fixed))
