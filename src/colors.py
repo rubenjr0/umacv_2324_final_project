cdff00629ff2b44eda03ee01a12f8bb541c1b231
Rubén <rubenjr0@protonmail.com>
2023-12-14 23:23:11 +0100

    🚧 Demo wip

--- src/colors.py
+++ src/colors.py
@@ -5,12 +5,21 @@ import numpy as np
 # red, orange and yellow can be very similar
 
 HSV_RANGES = {
-    "red": ((0, 60, 100), (9, 255, 255)),
-    "orange": ((10, 60, 100), (28, 255, 255)),
+    "red": ((0, 100, 100), (8, 255, 255)),
+    "orange": ((9, 60, 100), (28, 255, 255)),
     "yellow": ((29, 60, 100), (35, 255, 255)),
     "green": ((36, 70, 100), (85, 255, 255)),
     "blue": ((85, 100, 80), (135, 255, 255)),
     "white": ((0, 0, 200), (255, 60, 255)),
+}
+
+COLOR_CODES = {
+    "red": "R",
+    "orange": "O",
+    "yellow": "Y",
+    "green": "G",
+    "blue": "B",
+    "white": "W",
 }
 
 def _gamma_correction(channel: np.ndarray, gamma: float = 1.0) -> np.ndarray:
@@ -19,17 +28,8 @@ def _gamma_correction(channel: np.ndarray, gamma: float = 1.0) -> np.ndarray:
     return cv2.LUT(channel, table)
 
 def _get_hsv(rgb_image: np.ndarray) -> np.ndarray:
-    """Convert an RGB image to HSV
-
-    Args:
-        rgb_image (np.ndarray): An RGB image
-
-    Returns:
-        np.ndarray: An HSV image
-    """
-
     hsv = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2HSV)
-    hsv[..., 0] = hsv[..., 0] % 165
+    hsv[..., 0] = hsv[..., 0] % 170
     return hsv
 
 def _classify_color(piece: np.ndarray) -> str:
--- src/main.py
+++ src/main.py
@@ -1,10 +1,59 @@
 import cv2
 import numpy as np
-from colors import _gamma_correction
-from solver import solve
+from colors import COLOR_CODES, _gamma_correction
+from rubik.cube import Cube
+from rubik.solve import Solver
+from rubik.optimize import optimize_moves
 from vision import _fix_perspective, _get_face_colors, _preprocess
 
+
+
+def _dialog(dst, txt, line: int = 1, is_warped: bool = False):
+    base = 240 if is_warped else 16
+    cv2.putText(
+        dst,
+        txt,
+        (base, base + 16 * (line - (2 if is_warped else 1))),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        3 if is_warped else 0.25,
+         (0, 255, 0) if is_warped else (0, 255, 255),
+        4 if is_warped else 1,
+    )
+
+def _format_cube(cube: list[np.ndarray]) -> str:
+    top = [COLOR_CODES[c] for c in cube[0].flatten()]
+    left = cube[1]
+    front = cube[2]
+    right = cube[3]
+    back = cube[4]
+    bottom = [COLOR_CODES[c] for c in cube[5].flatten()]
+
+    # stack left, front, right, back horizontally
+    lfrb = np.hstack((left, front, right, back))
+    print(lfrb)
+    lfrb = lfrb.flatten()
+    lfrb = [COLOR_CODES[c] for c in lfrb]
+
+    # join top, lfbr, and bottom
+    cube = top + lfrb + bottom
+    return "".join(cube)
+
+
 cap = cv2.VideoCapture(0)
+cube = []
+solver = None
+moves = None
+saved_faces = []
+to_scan = ['top', 'left', 'front', 'right', 'back', 'bottom']
+t = 15
+did_save = False
+snapshot = None
+snapshot_face = None
+building_cube = True
+solving_cube = False
+manual_solve = False
+move_ptr = 0
+next_move = None
 
 while cap.isOpened():
     _, frame = cap.read()
@@ -13,21 +62,90 @@ while cap.isOpened():
     try:
         cropped, warped, M = _fix_perspective(frame)
         colors = _get_face_colors(cropped)
-        action = solve(colors)
 
-        # write action on warped, on (240, 240) from left to right
-        cv2.putText(
-            warped, str(action), (240, 240), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 4
-        )
+        face_color = colors[1, 1]
+
+        if manual_solve:
+            if face_color == saved_faces[2]:
+                _dialog(warped, next_move, is_warped=True)
+            else:
+                _dialog(frame, 'Please focus on the front face')
+
+        if building_cube and t == 0 and face_color not in saved_faces:
+            snapshot = colors
+            snapshot_face = face_color
 
         # undo perspective transform
         M_inv = np.linalg.inv(M)
-        unwarped = cv2.warpPerspective(warped, M_inv, (frame.shape[1], frame.shape[0]))
+        frame = cv2.warpPerspective(warped, M_inv, (frame.shape[1], frame.shape[0]))
 
     except Exception as _e:
-        continue
+        # print(_e)
+        pass
 
-    frame = cv2.cvtColor(unwarped, cv2.COLOR_BGR2RGB)
+    if building_cube:
+        if t == 0:
+            t = 15
+            did_save = False
+        else:
+            t -= 1
+
+        
+        _dialog(frame, f"Please scan the {to_scan[0]} face")
+
+        if not did_save and snapshot is not None:
+            _dialog(frame, f"Detected: {snapshot_face} face", line=2)
+            _dialog(frame, "Hold 's' to save", line=3)
+            for i in range(3):
+                _dialog(frame, str(snapshot[i, :]), line=6+i)
+
+        if cv2.waitKey(1) & 0xFF == ord("s") and snapshot is not None:
+            # append list elements instead of list
+            cube.append(snapshot)
+            saved_faces.append(snapshot_face)
+            to_scan.pop(0)
+            snapshot = None
+            if len(saved_faces) == 6:
+                building_cube = False
+                solving_cube = True
+                did_save = True
+            t = 60
+        
+        if did_save:
+            _dialog(frame, f"Saved: {snapshot_face} face")
+            snapshot = None
+    elif solving_cube:
+        building_cube = False
+        solving_cube = False
+        manual_solve = True
+        cube = _format_cube(cube)
+        cube = Cube(cube)
+        print('Solving cube...')
+        print(cube)
+        solver = Solver(cube)
+        solver.solve()
+        moves = solver.moves
+        moves = optimize_moves(moves)
+        print('Optimized moves:', moves)
+    elif manual_solve:
+        next_move = moves[move_ptr]
+        print('Next move:', next_move)
+        _dialog(frame, "Press 'a' to go back, 'd' to go forward")
+        if cv2.waitKey(1) & 0xFF == ord("a"):
+            if move_ptr > 0:
+                move_ptr -= 1
+        elif cv2.waitKey(1) & 0xFF == ord("d"):
+            if move_ptr < len(moves):
+                move_ptr += 1
+            else:
+                manual_solve = False
+                next_move = 'Solved!'
+                print('Solved cube!')
+
+        
+        
+
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     cv2.imshow("frame", frame)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
